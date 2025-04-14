70e35a2910d86409f39fbee4be0e51d091ab11cf
Pedro Leal <lealpedro2009@gmail.com>
2025-04-14 19:14:34 +0100

    feat: product detail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'products', loadComponent: () => import('./components/product-list/product-list.component').then(m => m.ProductListComponent) },
+  { path: 'product/:id', loadComponent: () => import('./components/product-item-detail/product-item-detail.component').then(m => m.ProductItemDetailComponent) },
+];
