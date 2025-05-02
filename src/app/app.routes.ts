2213439d16994fd89fcc348fd132a2f358072ccb
Pedro Leal <lealpedro2009@gmail.com>
2025-05-02 19:01:57 +0100

    add authentication

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,24 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/products', pathMatch: 'full' },
-  { path: 'products', loadComponent: () => import('./components/product-list/product-list.component').then(m => m.ProductListComponent) },
-  { path: 'product/:id', loadComponent: () => import('./components/product-item-detail/product-item-detail.component').then(m => m.ProductItemDetailComponent) },
+  {
+    path: 'products',
+    loadComponent: () =>
+      import('./components/product-list/product-list.component').then(
+        (m) => m.ProductListComponent
+      ),
+    canActivate: [() => import('./guard.guard').then((m) => m.guardGuard)],
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'product/:id',
+    loadComponent: () =>
+      import(
+        './components/product-item-detail/product-item-detail.component'
+      ).then((m) => m.ProductItemDetailComponent),
+  },
 ];
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  firstname: string = '';
+  lastname: string = '';
+  password: string = '';
+
+  constructor(
+    private http: HttpClient,
+    private auth: AuthService,
+    private router: Router
+  ) {
+    console.log('LoginComponent initialized with:', {
+      firstname: this.firstname,
+      lastname: this.lastname,
+      password: this.password,
+    });
+
+  }
+
+  login() {
+    this.auth
+      .login({
+        firstname: this.firstname,
+        lastname: this.lastname,
+        password: this.password,
+      })
+      .subscribe({
+        next: (res) => {
+          console.log('Login successful', res);
+          this.router.navigate(['/']);
+        },
+        error: (err) => {
+          console.error('Login failed', err);
+        },
+      });
+  }
+  debugLastName(value: any) {
+    console.log('LastName input:', value);
+    console.log('Type:', typeof value);
+  }
+}
