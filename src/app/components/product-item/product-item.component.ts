baa0fceec3690a8ca55fb3d42ccb05b1b02c0c8a
Pedro Leal <lealpedro2009@gmail.com>
2025-06-03 19:36:47 +0100

    feat: implement @Input and @Output decorators for component communication

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../../models/Product';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -20,12 +20,19 @@ export class ProductItemComponent {
     this.router.navigate(['/product', product.id]);
   }
   @Input() product!: Product;
+  @Output() addToCartEvent: EventEmitter<{ product: Product; quantity: number }> = new EventEmitter();
+
   quantity: number = 1;
 
   addToCart(product: Product, quantity: number) {
-    console.log('Product added to cart:', product);
-
+    // Emit the addToCartEvent with product and quantity
     event?.stopPropagation();
+    this.addToCartEvent.emit({ product, quantity });
+
+    // Optionally, you can also handle the cart addition logic here
+   /*  console.log('Product added to cart:', product);
+
+
     this.cartService.addToCart(product.id, quantity).subscribe({
       next: (response) => {
         console.log('Product added to cart successfully:', response);
@@ -34,6 +41,6 @@ export class ProductItemComponent {
       error: (error) => {
         console.error('Error adding product to cart:', error);
       },
-    });
+    }); */
   }
 }
--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -5,6 +5,7 @@ import { Product } from '../../models/Product';
 import { ProductItemComponent } from '../product-item/product-item.component';
 import { AuthService } from '../../services/auth.service';
 import { HttpClient } from '@angular/common/http';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-product-list',
@@ -15,22 +16,41 @@ import { HttpClient } from '@angular/common/http';
 export class ProductListComponent implements OnInit {
   makeTestCall() {
     this.authService.setToken('FAKE_TEST_TOKEN');
-  /*   this.http.get('https://httpbin.org/get').subscribe((response) => {
+    /*   this.http.get('https://httpbin.org/get').subscribe((response) => {
       console.log('Response:', response);
     }); */
-    this.http.get('/api/get').subscribe(response => {
+    this.http.get('/api/get').subscribe((response) => {
       console.log(response);
     });
   }
   products: Product[] = [];
 
-  constructor(private productService: ProductService, private authService: AuthService, private http: HttpClient) {}
+  constructor(
+    private productService: ProductService,
+    private authService: AuthService,
+    private http: HttpClient,
+    private cartService: CartService
+  ) {}
 
   ngOnInit(): void {
-
     this.productService.getProducts().subscribe((products) => {
       console.log(products);
       this.products = products;
+    });
+  }
+  handleAddToCart(event: { product: Product; quantity: number }) {
+    const { product, quantity } = event;
+    console.log('Product added to cart:', product, 'Quantity:', quantity);
+    // Here you can call your cart service to add the product to the cart
+    this.cartService.addToCart(product.id, quantity).subscribe({
+      next: (response) => {
+        console.log('Product added to cart successfully:', response);
+        alert('Product added to cart successfully');
+      },
+      error: (error) => {
+        console.error('Error adding product to cart:', error);
+
+      },
     });
   }
 }
